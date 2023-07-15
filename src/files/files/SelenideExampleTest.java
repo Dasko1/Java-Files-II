3340eca09a6b649d0afbe30cd1e16dce8bb3d0eb
Dasko <dasko2200@gmail.com>
2023-07-15 17:43:31 -0400

    Commit Java-Files-II files to directory

---
+++ src/files/files/SelenideExampleTest.java
@@ -0,0 +1,42 @@
+package files;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class SelenideExampleTest {
+
+    @BeforeAll
+    public static void setup() {
+        // Configure Selenide
+        Configuration.browser = "chrome";
+        Configuration.baseUrl = "https://example.com";
+    }
+
+    @Test
+    public void testPageTitle() {
+        // Open the webpage
+        Selenide.open("/");
+
+        // Get the page title
+        String pageTitle = Selenide.title();
+
+        // Assert the page title using AssertJ
+        Assertions.assertThat(pageTitle).isEqualTo("Example Domain");
+    }
+
+    @Test
+    public void testButtonVisibility() {
+        // Open the webpage
+        Selenide.open("/");
+
+        // Check if a button is visible
+        boolean isButtonVisible = Selenide.$("#button-id").isDisplayed();
+
+        // Assert the visibility using AssertJ
+        Assertions.assertThat(isButtonVisible).isFalse();
+    }
+}
+
---
+++ src/files/files/StringArrayExample.java
@@ -0,0 +1,18 @@
+package files;
+
+import org.testng.Assert;
+
+public class StringArrayExample {
+
+    public static void main(String[] args){
+
+        String[] stringArray={"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+
+        for (String s : stringArray) {
+            System.out.println(s);
+
+            Assert.assertEquals("Friday", "Friday");
+        }
+
+    }
+}
