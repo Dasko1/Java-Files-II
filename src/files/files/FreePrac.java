255535b5ac688fa53906857c8319fd08395b7b73
Dasko <dasko2200@gmail.com>
2024-04-13 19:52:31 -0400

    Commit new Java-Files-II files to main

---
+++ src/files/files/FreePrac.java
@@ -0,0 +1,15 @@
+package files;
+
+public class FreePrac {
+
+    public static void main(String[] args){
+
+        int[] numbers = {1,2,3,4,5};
+        int i = 1;
+
+        while (i <= 5){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
