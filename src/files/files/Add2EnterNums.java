e607c6a5d19bc7fca5c42570320aaf6525e1ac37
Dasko <dasko2200@gmail.com>
2024-04-14 12:45:06 -0400

    Push updated files!

--- src/files/files/Add2EnterNums.java
+++ src/files/files/Add2EnterNums.java
@@ -7,15 +7,14 @@ public class Add2EnterNums {
     public static void main(String[] args){
 
         Scanner nums = new Scanner(System.in);                 // Create a Scanner object!
-        System.out.println("Enter value for x: ");
+
+        System.out.print("Enter value for x: ");
         int x = nums.nextInt();
-        System.out.println("Enter value for y: ");
+        System.out.print("Enter value for y: ");
         int y = nums.nextInt();
 
         int z = x + y;
 
         System.out.println("The sum is: " + z);
     }
-
-
 }
