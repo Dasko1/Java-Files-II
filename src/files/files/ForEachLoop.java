f23082a255b31b8639373f29b8123aa091530ea0
Dasko <dasko2200@gmail.com>
2025-04-21 17:55:44 -0400

    Added a for each loop, or Enhanced for loop, as some call it.

---
+++ src/files/files/ForEachLoop.java
@@ -0,0 +1,13 @@
+package files;
+
+public class ForEachLoop{
+
+    public static void main(String[] args){
+
+        int[] numbers = {1,2,3,4,5};
+
+        for (int nums : numbers){
+            System.out.println(nums);
+        }
+    }
+}
